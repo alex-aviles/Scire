4cae8efd334b6661192d8dfd645c4418cbc86660
Alexander Aviles <131209543+alex-aviles@users.noreply.github.com>
2025-06-19 22:42:47 -0700

    banner

---
+++ scire/scire.py
@@ -0,0 +1,11 @@
+#Scire - Information Gathering, Recon, Scanning, and Enumeration
+
+banner = """\033[91m
+ _______ _______ _____  ______ _______
+ |______ |         |   |_____/ |______
+ ______| |_____  __|__ |    \_ |______
+
+ \033[0m"""
+ 
+print(banner)
+
